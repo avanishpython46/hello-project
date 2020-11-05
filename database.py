0ca2ae230045dc758dccf33e9b2ca2e04e9884e7
avanishpython46 <73603525+avanishpython46@users.noreply.github.com>
2020-11-05 23:07:06 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,24 @@
+lst = []
+
+def add_book():
+  name = input("Enter the name of the book : ")
+  author = input("Enter the author : ")
+  lst.append({'name':name,'Author':author,'read':False})
+
+
+def list_book():
+  print(lst)
+
+
+def delete():
+  index = int(input("Enter the index : "))
+  del lst[index]
+
+
+def read_book():
+  b = int(input("Enter the index : "))
+  l = lst[b]
+  for ele in l:
+    f=l['read']=True
+  with open('file.txt','w') as f:
+    f.write(str(l))
